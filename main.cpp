28bc7ce0250821298d2c85c6488b40c5e066b1ff
5249851 <92885062+5249851@users.noreply.github.com>
2021-11-15 01:42:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   
+   int n=0, primo=1, secondo=1, terzo=0;
+   cout<<"inserisci numero"<<endl;
+   cin>>n;
+   
+   if(n>2){
+      cout<<primo<<endl;
+      cout<<secondo<<endl;
+      
+   for(int i=2;i<n;i++){
+      terzo=primo+secondo;
+      primo=secondo;
+      cout<<terzo<<endl;
+   }
+   
+   }
+   else{
+      cout<<"errore"<<endl;
+   }
    return 0;
 }
